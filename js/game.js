730b1ba2fa88774945a79cd5b6054b114d3c89ad
Artem Kushch <artemkusch2@gmail.com>
2024-12-26 07:04:31 +0200

    Implement basic game loop with piece dropping

--- js/game.js
+++ js/game.js
@@ -1,8 +1,31 @@
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
+let board = new Board();
+let currentPiece = null;
+let dropCounter = 0;
+let dropInterval = 1000;
+let lastTime = 0;
+
+function update(time = 0) {
+    const deltaTime = time - lastTime;
+    lastTime = time;
+    dropCounter += deltaTime;
+
+    if (dropCounter > dropInterval) {
+        currentPiece.y++;
+        dropCounter = 0;
+    }
+
+    draw();
+    requestAnimationFrame(update);
+}
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    board.draw();
+    if (currentPiece) {
+        currentPiece.draw();
+    }
 }
 
-draw();
+// Start game
+currentPiece = new Piece(PIECES[0], 1);
+update();
